ea21dd33c359a11537fa46434ad3352bd994248f
Aloys Berger <aloysberger@gmail.com>
2020-04-03 13:22:15 +0200

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://aloysberger:viriat12@cluster0-uuctx.azure.mongodb.net/test?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://aloysberger:viriat12@cluster0-uuctx.azure.mongodb.net/test?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
